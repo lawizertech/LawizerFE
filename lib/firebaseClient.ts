59e1604345b494a035931abbbde2ed144c5eca9e
NayanPatidar <nayanpatidar28@gmail.com>
2026-01-02 15:11:04 +0530

    getDb from Firebase

--- lib/firebaseClient.ts
+++ lib/firebaseClient.ts
@@ -1,6 +1,7 @@
 // lib/firebaseClient.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
@@ -9,7 +10,9 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
+export const rtdb = getDatabase(app);
