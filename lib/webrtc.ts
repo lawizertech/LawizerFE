7a4f6250541f95c6a7018d62b65f1f57d3e54dd9
NayanPatidar <nayanpatidar28@gmail.com>
2026-01-03 17:19:54 +0530

    expressturn turn server config

--- lib/webrtc.ts
+++ lib/webrtc.ts
@@ -1,6 +1,11 @@
 export const iceServers: RTCIceServer[] = [
   { urls: "stun:stun.l.google.com:19302" },
   { urls: "stun:stun1.l.google.com:19302" },
+  {
+    urls: "turn:relay1.expressturn.com:3480",
+    username: "000000002082800792",
+    credential: "JPGMR7t+o9ieAB4KFh6t4bhtta8=",
+  },
 ];
 
 export function createPeerConnection(
