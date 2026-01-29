40e7b8d8f309642a01ba033218a6ac917244c26f
NayanPatidar <nayanpatidar28@gmail.com>
2026-01-29 13:52:27 +0530

    User Dashboard Renderer

---
+++ components/user/UserDashboardRenderer.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import UserDashboard from "./DashboardTab";
+import MyConsultationsTab from "./MyConsultationsTab";
+import ActiveServicesTab from "./ActiveServicesTab";
+
+export default function UserDashboardRenderer() {
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab") || "dashboard";
+
+  return (
+    <>
+      {tab === "dashboard" && <UserDashboard />}
+      {tab === "consultations" && <MyConsultationsTab />}
+      {tab === "services" && <ActiveServicesTab />}
+    </>
+  );
+}
